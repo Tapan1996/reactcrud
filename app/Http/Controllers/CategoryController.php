3d986e9a706724b4b462d7d75c72e3f49ca86484
tapan1996 <malejduffy@seznam.cz>
2023-10-12 18:26:02 +0200

    controllers with functionality

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,63 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the categories.
      */
     public function index()
     {
-        //
+        try {
+            return response(['success' => true, 'data' => Category::all(), 'message' => null]);
+        } catch (\Throwable $exception) {
+            return response(['success' => true, 'data' => null, 'message' => $exception]);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created category in db.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+            ]);
+
+            if ($validator->fails()) {
+                return response(['success' => false, 'data' => null, 'message' => $validator->errors()]);
+            }
+
+            $category = new Category();
+            $category->name = $request->input('name');
+            $category->save();
+            return response(['success' => true, 'data' => $category, 'message' => 'Category created']);
+
+        } catch (\Exception $exception) {
+            return response(['success' => false, 'data' => null, 'message' => $exception]);
+
+        }
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified category.
      */
     public function show(string $id)
     {
-        //
+        try {
+            $category = Category::findOrFail($id);
+            return response(['success' => true, 'data' => $category, 'message' => null]);
+        } catch (\Throwable $exception) {
+            return response(['success' => true, 'data' => null, 'message' => $exception]);
+        }
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified category.
      */
-    public function edit(string $id)
+    public function edit(string $id, Request $request)
     {
         //
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified category in db.
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+            ]);
+
+            if ($validator->fails()) {
+                return response(['success' => false, 'data' => null, 'message' => $validator->errors()]);
+            }
+
+            $category = Category::findOrFail($id);
+            $category->name = $request->input('name');
+            $category->save();
+            return response(['success' => true, 'data' => $category, 'message' => 'Category updated']);
+
+        } catch (\Exception $exception) {
+            return response(['success' => false, 'data' => null, 'message' => $exception]);
+
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified category from db.
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $category = Category::findOrFail($id);
+            $category->delete();
+            return response(['success' => true, 'data' => null, 'message' => 'Category deleted']);
+        } catch (\Throwable $exception) {
+            return response(['success' => true, 'data' => null, 'message' => $exception]);
+        }
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,20 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the products.
      */
     public function index()
     {
-        //
+        try {
+            return response(['success' => true, 'data' => Product::with('category')->get(), 'message' => null]);
+        } catch (\Throwable $exception) {
+            return response(['success' => true, 'data' => null, 'message' => $exception]);
+        }
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new product.
      */
     public function create()
     {
@@ -23,23 +30,50 @@ class ProductController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created product in db.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'price' => 'required|decimal:0,2|max:9999999',
+                'category_id' => 'required|int',
+
+            ]);
+
+            if ($validator->fails()) {
+                return response(['success' => false, 'data' => null, 'message' => $validator->errors()]);
+            }
+
+            $product = new Product();
+            $product->name = $request->input('name');
+            $product->price = $request->input('price');
+            $product->category_id = $request->input('category_id');
+            $product->save();
+            return response(['success' => true, 'data' => $product, 'message' => 'Product created']);
+
+        } catch (\Throwable $exception) {
+            return response(['success' => false, 'data' => null, 'message' => $exception]);
+
+        }
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified product.
      */
     public function show(string $id)
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            return response(['success' => true, 'data' => $product, 'message' => null]);
+        } catch (\Throwable $exception) {
+            return response(['success' => true, 'data' => null, 'message' => $exception]);
+        }
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified product.
      */
     public function edit(string $id)
     {
@@ -47,18 +81,46 @@ class ProductController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified product in db.
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'price' => 'required|decimal:0,2|max:9999999',
+                'category_id' => 'required|int',
+
+            ]);
+
+            if ($validator->fails()) {
+                return response(['success' => false, 'data' => null, 'message' => $validator->errors()]);
+            }
+
+            $product = Product::findOrFail($id);
+            $product->name = $request->input('name');
+            $product->price = $request->input('price');
+            $product->category()->save(Category::findOrFail($request->input('category_id')));
+            $product->save();
+            return response(['success' => true, 'data' => $product, 'message' => 'Product updated']);
+
+        } catch (\Throwable $exception) {
+            return response(['success' => false, 'data' => null, 'message' => $exception]);
+
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified product from db.
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            $product->delete();
+            return response(['success' => true, 'data' => null, 'message' => 'Product deleted']);
+        } catch (\Throwable $exception) {
+            return response(['success' => true, 'data' => null, 'message' => $exception]);
+        }
     }
 }
